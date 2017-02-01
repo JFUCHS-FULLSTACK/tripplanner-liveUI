cf4965f1e016729eb39c68de08bc6db5d5be0aed
Jonathan Fuchs <jgfuchs@gmail.com>
2017-02-01 12:01:15 -0500

    figured out how to remove markers

--- public/js/map.js
+++ public/js/map.js
@@ -46,6 +46,10 @@ $(function initializeMap (){
     activity: '/images/star-3.png'
   };
 
+
+  let markerArray = [];
+
+
   function drawMarker (type, coords) {
     var latLng = new google.maps.LatLng(coords[0], coords[1]);
     var iconURL = iconURLs[type];
@@ -54,6 +58,20 @@ $(function initializeMap (){
       position: latLng
     });
     marker.setMap(currentMap);
+
+    markerArray.push([marker, coords[0], coords[1]]);
+  }
+
+  function deleteMarker (type, coords){
+
+    var marker = markerArray.filter(function(elem){
+      console.log('elem1:', elem[1], 'coords0:', coords[0])
+      return Number(elem[1]) === Number(coords[0])
+
+    })
+
+    marker[0][0].setMap(null);
+
   }
 
   // drawMarker('hotel', [40.705137, -74.007624]);
@@ -79,16 +97,11 @@ $(function initializeMap (){
         drawMarker('hotel', janky.split(','));
       }
     }
+  });
 
-
-
-
-
-    // console.log(hotelLocationArray.filter(function(elem){return elem.id === $hotelName})); //and then filter the <a> elements that have the ID that equlas the name of the hotel that we selected. and then get the value of that element
 
 
 let restaurantClickCount = 0;
-let randomCounterThignForTesting = 0;
 $('#restaurant-choices-btn').on('click', function(evt) {
     evt.preventDefault();
     const $restaurantName = $('#restaurant-choices').val();
@@ -96,14 +109,14 @@ $('#restaurant-choices-btn').on('click', function(evt) {
       $('#restaurant-itinerary').empty();
       restaurantClickCount++;
     }
-
-
-    const $newRestaurant = $('<div class="itinerary-item"><li class="title" id="counter' + randomCounterThignForTesting + '">' + $restaurantName + '</li><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
-    randomCounterThignForTesting++;
-  $('#restaurant-itinerary').append($newRestaurant);
-    console.log($restaurantName);
+    const $newActivity = $('<div class="itinerary-item"><li class="title">' + $restaurantName + '</li><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+  $('#restaurant-itinerary').append($newActivity);
 });
 
+$('#restaurant-itinerary').on('click', '.remove', function(evt) {
+
+    $(this).parent().remove();
+});
 
 
 let activityClickCount = 0;
@@ -118,13 +131,89 @@ $('#activity-choices-btn').on('click', function(evt) {
   $('#activity-itinerary').append($newActivity);
 });
 
+$('#activity-itinerary').on('click', '.remove', function(evt) {
+    $(this).parent().remove();
+});
+
+
+let addDayCounter = 4;
+
+$('#day-add').click(function(){
+  $this = $(this)
+  $this.parent().append('<button class="btn btn-circle day-btn day'+addDayCounter+'" style="margin-right: 3px">'+addDayCounter+'</button>')
+  $this.parent().append($this);  // $(this).remove()
+  addDayCounter++;
+  deleteMarker('hotel', [40.705137, -74.007624]);
+
+})
+
+
 });
 
 
 
+/*
+
+how to delete marker:
+
+1. when we create the div for the hotel or whatever, we have access to the coordinates.
+2. with that coordinates, we are going to make the remove button include the coordinates somehow and input it into the deleteMarker function.
+*/
+
+
+/*
+our plan to switch days using HTML stuff
+1. create a parent <span> element for each day, and give it a corresponding id (i.e. #day1)
+2. when we select the right day button, it hides all of the other spans and displays the selected one/or create it if it doesnt exist yet
+  a. hide all spans
+  b. display span id=day#
+*/
 
 
 
+
+
+
+
+// $(function () {
+//     var counter = 0;
+
+//     $('a').click(function () {
+//         var elems = '<div>'+
+//               '<input type="checkbox" id="checkbox"' + (counter) + '" class="item"/>' + 
+//               '<input type="text" id="t1"' + (counter) + '"/>' +
+//               '<input type="button" class="removebtn" value="." id="removebtn"' + (counter) + '"/>' +
+//         '</div>';
+//         $('#box').append(elems);
+//         $("#box").append("<br />");
+//         $("#box").append("<br />");
+//         counter++;
+//     });
+
+//     $('.removebtn').live(function () {
+//         $(this).parent().remove();   
+//     });
+// });
+
+ 
+
+
+
+
+
+// $('#restaurant-choices-btn').on('click', function(evt) {
+//     evt.preventDefault();
+//     const $restaurantName = $('#restaurant-choices').val();
+//     if (restaurantClickCount === 0){
+//       $('#restaurant-itinerary').empty();
+//       restaurantClickCount++;
+//     }
+//     const $newRestaurant = $('<div class="itinerary-item"><li class="title" id="restaurantID' + randomCounterThingForTesting + '">' + $restaurantName + '</li><button data-action:"remove" class="btn btn-xs btn-danger remove btn-circle" id="restRemoveBtn' + randomCounterThingForTesting + '">x</button></div>');   
+
+//     randomCounterThingForTesting++;
+//   $('#restaurant-itinerary').append($newRestaurant);
+//     console.log($restaurantName);
+// });
 
 
 
